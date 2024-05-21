1c826f272c1cca86a3314bd5a391e81162c04149
Núbia Almeida <104798486+Nubia-almeida@users.noreply.github.com>
2024-05-21 15:26:32 -0300

    Add files via upload

---
+++ src/components/button/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const ButtonContainer= styled.button`
+    border: 1px solid red;
+    background-color: #00AAF0;
+    padding: 18px; 
+    font-size: 24px;
+    font-weigth: 700;
+    margin: 1px 0;
+    flex:1;
+
+    &:hover{
+        opacity: 0.6;
+    }
+`
